c01f217951cdd7ba58bbf1e985805731d097c6f1
Joerg Wangemann <joerg.wangemann@gmx.de>
2016-11-06 20:09:14 +0100

    Considering variable frequency FRT-period.

--- bldc_clock_calculations.m
+++ bldc_clock_calculations.m
@@ -1,3 +1,4 @@
+clear;
 % STM32F4 Clock Tree
 f_HSE = 8e6; % HSE clock frequency
 STM32_PLLM_VALUE = 6;
@@ -14,51 +15,57 @@ Ks_ADC_conversion = 12;% ADC resolution / ADCCLK cycles for conversion
 Ks_ADC = Ks_ADC_samplehold + Ks_ADC_conversion
 disp('ADC clock frequency (check against max value in datasheet!)')
 f_ADCCLK = f_STM32_SYSCLK / STM32_PPRE2 / STM32_ADC_ADCPRE
-f_ADC = f_ADCCLK / Ks_ADC
-ADC_COMMUTATE_FREQUENCY = f_ADC;
+f_ADC = f_ADCCLK / Ks_ADC;
+ADC_COMMUTATE_FREQUENCY = f_ADC
 
 Ts_ADC1 = 1 / f_ADC; % Sample time of ADC [s]
-ADC_COMMUTATE_NUM_CHANNELS = 1;
-ADC_COMMUTATE_BUF_DEPTH = 50;
-f_ADC_single = 1 / (Ts_ADC1 * ADC_COMMUTATE_NUM_CHANNELS) % Sample frequency of each channel
-T_cb_ADC1 = Ts_ADC1 * ADC_COMMUTATE_NUM_CHANNELS * ADC_COMMUTATE_BUF_DEPTH / 2 % Period of ADC1 streaming callback
-f_cb_ADC1 = 1 / T_cb_ADC1
+
 
 % PWM configuration
 ADC_PWM_DIVIDER = Ks_ADC * STM32_ADC_ADCPRE % When f_TIM1 = f_TIM2 (*STM32_PPRE2 for f_TIM1 = f_SYSCLK)
-% PWM clock ticks per ADC samples
 
-f_PWM = 20e3;
-duty_cycle = 0.6;
-t_on = duty_cycle * 1/f_PWM
-samples_when_on = t_on * f_single
 
 disp('Schaltzeiten')
 PWM_CLOCK_FREQUENCY = f_STM32_SYSCLK / 2;
 
-PWM_DEFAULT_PERIOD_CYCLES = ADC_PWM_DIVIDER * 20; % Choose a number of ADC samples in a PWM period
+ADC_FRT_DEFAULT_PERIOD_CYCLES = 50; % Fastest FRT cycle period in ADC cycles
+ADC_NUM_CHANNELS = 1;
+PWM_ADC_DEFAULT_PERIOD_CYCLES = 20;  % Choose a default number of ADC samples in a PWM period
+PWM_ADC_MAXIMUM_PERIOD_CYCLES = 100; % Choose a maximum number of ADC samples in a PWM period (determines maximum PWM period)
+ADC_COMMUTATE_BUF_DEPTH = ADC_NUM_CHANNELS * PWM_ADC_MAXIMUM_PERIOD_CYCLES * 2
+PWM_DEFAULT_PERIOD_CYCLES = ADC_NUM_CHANNELS * ADC_PWM_DIVIDER * PWM_ADC_DEFAULT_PERIOD_CYCLES;
+PWM_MAXIMUM_PERIOD_CYCLES = ADC_NUM_CHANNELS * ADC_PWM_DIVIDER * PWM_ADC_MAXIMUM_PERIOD_CYCLES;
 
+FRT_DEFAULT_FREQUENCY = PWM_CLOCK_FREQUENCY / (ADC_PWM_DIVIDER * ADC_FRT_DEFAULT_PERIOD_CYCLES)
 PWM_DEFAULT_FREQUENCY = PWM_CLOCK_FREQUENCY / PWM_DEFAULT_PERIOD_CYCLES % [40e3, 50e3, 62500, 100e3]	choose one of these base frequencies [Hz]
-PWM_MINIMUM_FREQUENCY = 40000;
+PWM_MINIMUM_FREQUENCY = PWM_CLOCK_FREQUENCY / PWM_MAXIMUM_PERIOD_CYCLES
 
 %ADC_COMMUTATE_FREQUENCY	= 1e6;%		// [Hz]
-ADC_PWM_DIVIDER = (PWM_CLOCK_FREQUENCY / ADC_COMMUTATE_FREQUENCY)
-ADC_PWM_PERIOD = (ADC_COMMUTATE_FREQUENCY / PWM_DEFAULT_FREQUENCY); % In ticks
-PWM_MAXIMUM_PERIOD = (ADC_PWM_DIVIDER * ADC_COMMUTATE_FREQUENCY / PWM_MINIMUM_FREQUENCY);
+% ADC_PWM_DIVIDER = (PWM_CLOCK_FREQUENCY / ADC_COMMUTATE_FREQUENCY)
+% ADC_PWM_PERIOD = (ADC_COMMUTATE_FREQUENCY / PWM_DEFAULT_FREQUENCY) % In ADC-ticks
+% PWM_MAXIMUM_PERIOD = (ADC_PWM_DIVIDER * ADC_COMMUTATE_FREQUENCY / PWM_MINIMUM_FREQUENCY);
 
+
+% Calculate the PWM-period with respect to the duty cycle
 d_percent = (1:49) + 49;
-pwm_period = fix( ((ADC_PWM_PERIOD * 2500) ./ ((100 .- d_percent) .* d_percent))) .* ADC_PWM_DIVIDER;
+pwm_period = fix( ((PWM_ADC_DEFAULT_PERIOD_CYCLES * 2500) ./ ((100 .- d_percent) .* d_percent))) .* (ADC_NUM_CHANNELS * ADC_PWM_DIVIDER);
 
-pwm_frequency = PWM_CLOCK_FREQUENCY ./ pwm_period
+pwm_frequency = PWM_CLOCK_FREQUENCY ./ pwm_period;
 
 for i=1:length(pwm_period)
-  if pwm_period(i) > PWM_MAXIMUM_PERIOD
-    pwm_period(i) = PWM_MAXIMUM_PERIOD;
+  if pwm_period(i) > PWM_MAXIMUM_PERIOD_CYCLES
+    pwm_period(i) = PWM_MAXIMUM_PERIOD_CYCLES;
+  endif
+  frt_period(i) = pwm_period(i);
+  if frt_period(i) < (ADC_FRT_DEFAULT_PERIOD_CYCLES * ADC_PWM_DIVIDER)
+    frt_period(i) = ( fix((ADC_FRT_DEFAULT_PERIOD_CYCLES * ADC_PWM_DIVIDER) / pwm_period(i)) + 1 ) * pwm_period(i);
   endif
 endfor
 
 t_on = d_percent ./ 100 .* pwm_period;
 t_off = pwm_period - t_on;
-plot(d_percent, pwm_period, d_percent, t_off)
+plot(d_percent, frt_period, d_percent, pwm_period, d_percent, t_off)
+xlabel('Duty cycle, d [%]');
+legend('FRT-Period', 'PWM-Period', 't_{off}');
 % plot(d_percent, t_off)
 % Check max samples for linear fit;
