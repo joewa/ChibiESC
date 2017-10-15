6b0597070316189fa6db349f16d3b49e698f858b
Heiko <heiko-r@users.noreply.github.com>
2017-10-15 19:01:17 +0200

    Fix USB configuration for Looong Strip F4
    Looong Strip F4 boards do not have the USB-VBUSSENS line. Board.h was not fully updated to ..._F4 board names.

--- board/board.h
+++ board/board.h
@@ -79,7 +79,7 @@
 #endif
 
 // Looong-Strip and VESC do not have USB-VBUSSENS wire connected (never master)
-#if defined(BOARD_LOOONG_STRIP) || defined(BOARD_VESC)
+#if defined(BOARD_LOOONG_STRIP_F4) || defined(BOARD_VESC)
 // TODO: NRT function blocks as long as USB is NOT connected!
 #define BOARD_OTG_NOVBUSSENS
 #endif
